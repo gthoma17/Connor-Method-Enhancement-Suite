0be85b9047d842d4af0983e2f4f7bd5cdf424a61
Greg Thomas <gthoma17@emich.edu>
2015-04-17 15:27:01 -0400

    Added delay between tab openings.

--- content.js
+++ content.js
@@ -1,14 +1,19 @@
+matchingURLs = [];
 chrome.storage.sync.get({
     urlPattern: 'https://www.linkedin.com/profile',
   }, function(items) {
   	var pattern = items.urlPattern;
   	console.log("Opening URLs matching pattern: " + pattern);
   	var allURLs = document.getElementsByTagName("a");
+    var matchingURLs = [];
   	for (var i = allURLs.length - 1; i >= 0; i--) {
   		//console.log(allURLs[i].href);
   		if (allURLs[i].href.match(pattern) != null) {
   			console.log("Match: " + allURLs[i].href);
-  			window.open(allURLs[i].href,'_blank');
+        matchingURLs.push(allURLs[i].href)
+  			//window.open(allURLs[i].href,'_blank');
+        //window.open(matchingURLs.pop(),'_blank');
+    setInterval(function(){ window.open(matchingURLs.pop(),'_blank'); }, 1500);
   		};
   	};
   });
