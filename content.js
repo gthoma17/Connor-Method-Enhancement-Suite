cd3a645b0a7061802ccf4ff96398312466a7fe47
Greg Thomas <gthoma17@emich.edu>
2015-04-20 14:32:47 -0400

    Implemented ignore patterns

--- content.js
+++ content.js
@@ -1,21 +1,51 @@
 chrome.storage.sync.get({
     urlPattern: 'https://www.linkedin.com/profile',
+    ignorePattern: '',
     delay: '10000',
   }, function(items) {
   	var pattern = items.urlPattern;
+    var badPattern = items.ignorePattern;
     var delay = items.delay;
   	console.log("Opening URLs matching pattern: " + pattern);
+    console.log("Unless they match pattern: " + badPattern);
   	var allURLs = document.getElementsByTagName("a");
-    var matchingURLs = [];
+    var possibleURLs = [];
   	for (var i = allURLs.length - 1; i >= 0; i--) {
   		if (allURLs[i].href.match(pattern) != null) {
   			console.log("Match: " + allURLs[i].href);
-        matchingURLs.push(allURLs[i].href)
-  			//window.open(allURLs[i].href,'_blank');
-        //window.open(matchingURLs.pop(),'_blank');
+        possibleURLs.push(allURLs[i].href)
       };
     };
+    matchingURLs = pruneURLs(possibleURLs,badPattern); //this is where site specific url pruning should occur
     console.log("#of Matching links: " + matchingURLs.length);
     console.log("Interval between openings (ms): " + parseInt(delay));
-    setInterval(function(){if (matchingURLs.length>0) {window.open(matchingURLs.pop(),'_blank');};}, parseInt(delay));
+    setInterval(function(){openURLs();}, parseInt(delay));
   });
+function openURLs(){
+  if (matchingURLs.length>0) {
+    var thisUrl = matchingURLs.pop();
+    console.log("Opening: " + thisUrl + " " + matchingURLs.length + " links remaining");
+    window.open(thisUrl,'_blank');
+  };
+}
+function pruneURLs(array,pattern){
+  var profile_ids = [];
+  for (var i = array.length - 1; i >= 0; i--) {
+    if(array[i].match(pattern) != null){
+      console.log("Removing: " + array[i] + " ...matches ignore pattern");
+      array.splice(i,1);
+    }
+    else{
+      profileStart = array[i].indexOf("view?id=")+8;
+      profileEnd = array[i].indexOf("&",profileStart);
+      profile = array[i].substring(profileStart,profileEnd);
+      if (profile_ids.indexOf(profile) != -1) {
+        array.splice(i,1);
+        console.log("Removing: " + array[i] + " ...already in array");
+      } else{
+        profile_ids.push(profile)
+      };
+    };
+  };
+  return array;
+}
--- options.js
+++ options.js
@@ -1,9 +1,11 @@
 // Saves options to chrome.storage
 function save_options() {
   var url_pattern = document.getElementById('url_pattern').value;
+  var ignore_pattern = document.getElementById('ignore_pattern').value;
   var delay_time = document.getElementById('delay_length').value;
   chrome.storage.sync.set({
     urlPattern: url_pattern,
+    ignorePattern: ignore_pattern,
     delay: delay_time,
   }, function() {
     // Update status to let user know options were saved.
@@ -18,12 +20,14 @@ function save_options() {
 
 
 function restore_options() {
-  // Use default value url_pattern = '.*linkedin.com/profile.*'.
+  // Use default value url_pattern = 'https://www\.linkedin\.com/profile/view\?id=.*.
   chrome.storage.sync.get({
-    urlPattern: '.*linkedin.com/profile.*',
+    urlPattern: '.*linkedin.com/profile/.*',
+    ignorePattern: '',
     delay: '10000'
   }, function(items) {
     document.getElementById('url_pattern').value = items.urlPattern;
+    document.getElementById('ignore_pattern').value = items.ignorePattern;
     document.getElementById('delay_length').value = items.delay;
   });
 }
