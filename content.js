ad8802e1313654bc6bdfc8546054683a6e8dfeb2
Greg Thomas <gthoma17@emich.edu>
2015-04-22 11:15:19 -0400

    Implemented anchor class pattern matching

--- content.js
+++ content.js
@@ -2,24 +2,29 @@ chrome.storage.sync.get({
     urlPattern: 'https://www.linkedin.com/profile',
     ignorePattern: '',
     delay: '10000',
+	  classPattern: '.*',
   }, function(items) {
   	var pattern = items.urlPattern;
     var badPattern = items.ignorePattern;
+	  var classPattern = items.classPattern;
     var delay = items.delay;
   	console.log("Opening URLs matching pattern: " + pattern);
-    console.log("Unless they match pattern: " + badPattern);
+    console.log("--Where the class matches" + classPattern)
+    console.log("--Unless they match pattern: " + badPattern);
   	var allURLs = document.getElementsByTagName("a");
     var possibleURLs = [];
   	for (var i = allURLs.length - 1; i >= 0; i--) {
-  		if (allURLs[i].href.match(pattern) != null) {
+  		if (allURLs[i].href.match(pattern) != null &&
+          allURLs[i].className.match(classPattern) != null &&
+          allURLs[i].href.match(badPattern) == null) {
   			console.log("Match: " + allURLs[i].href);
-        possibleURLs.push(allURLs[i].href)
-      };
-    };
+				possibleURLs.push(allURLs[i].href);
+			};
+		};
     matchingURLs = pruneURLs(possibleURLs,badPattern); //this is where site specific url pruning should occur
     console.log("#of Matching links: " + matchingURLs.length);
     console.log("Interval between openings (ms): " + parseInt(delay));
-    setInterval(function(){openURLs();}, parseInt(delay));
+    setInterval(function(){openURLs();}, parseInt(delay)); //Commented out
   });
 function openURLs(){
   if (matchingURLs.length>0) {
@@ -28,23 +33,17 @@ function openURLs(){
     window.open(thisUrl,'_blank');
   };
 }
-function pruneURLs(array,pattern){
+function pruneURLs(array){
   var profile_ids = [];
   for (var i = array.length - 1; i >= 0; i--) {
-    if(array[i].match(pattern) != null){
-      console.log("Removing: " + array[i] + " ...matches ignore pattern");
+    profileStart = array[i].indexOf("view?id=")+8;
+    profileEnd = array[i].indexOf("&",profileStart);
+    profile = array[i].substring(profileStart,profileEnd);
+    if (profile_ids.indexOf(profile) != -1) {
       array.splice(i,1);
-    }
-    else{
-      profileStart = array[i].indexOf("view?id=")+8;
-      profileEnd = array[i].indexOf("&",profileStart);
-      profile = array[i].substring(profileStart,profileEnd);
-      if (profile_ids.indexOf(profile) != -1) {
-        array.splice(i,1);
-        console.log("Removing: " + array[i] + " ...already in array");
-      } else{
-        profile_ids.push(profile)
-      };
+      console.log("Removing: " + array[i] + " ...already in array");
+    } else{
+      profile_ids.push(profile)
     };
   };
   return array;
--- options.js
+++ options.js
@@ -3,10 +3,12 @@ function save_options() {
   var url_pattern = document.getElementById('url_pattern').value;
   var ignore_pattern = document.getElementById('ignore_pattern').value;
   var delay_time = document.getElementById('delay_length').value;
+  var class_pattern = document.getElementById('class_pattern').value;
   chrome.storage.sync.set({
     urlPattern: url_pattern,
     ignorePattern: ignore_pattern,
     delay: delay_time,
+	  classPattern: class_pattern,
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -22,13 +24,15 @@ function save_options() {
 function restore_options() {
   // Use default value url_pattern = 'https://www\.linkedin\.com/profile/view\?id=.*.
   chrome.storage.sync.get({
-    urlPattern: 'https://www.linkedin.com/profile/view?id=.*',
-    ignorePattern: '',
-    delay: '10000'
+    urlPattern: '.*linkedin.com/profile/view.*',
+    ignorePattern: '(?=a)b', //this regex will never match
+    delay: '10000',
+	  classPattern: ".*", //this regex will always match
   }, function(items) {
     document.getElementById('url_pattern').value = items.urlPattern;
     document.getElementById('ignore_pattern').value = items.ignorePattern;
     document.getElementById('delay_length').value = items.delay;
+	document.getElementById('class_pattern').value = items.classPattern;
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
